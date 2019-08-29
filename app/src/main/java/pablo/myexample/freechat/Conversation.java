56a5240188b1f21a4379a5216593cecd8d606a11
PabloLopez1998 <35881455+PabloLopez1998@users.noreply.github.com>
2019-08-29 03:32:50 -0700

    Up Next: Finish up conversation chat room

--- app/src/main/java/pablo/myexample/freechat/Conversation.java
+++ app/src/main/java/pablo/myexample/freechat/Conversation.java
@@ -1,5 +1,6 @@
 package pablo.myexample.freechat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,13 +16,22 @@ import android.view.MenuInflater;
 import android.view.SubMenu;
 import android.widget.Toolbar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class Conversation extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
     private MessageAdapter messageAdapter;
     private ArrayList<MessageObject> arrayList;
     private Intent intent;
+    private DatabaseReference databaseReference;
+    private ArrayList<String> names, ids;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +40,53 @@ public class Conversation extends AppCompatActivity {
         setTitle(" ");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //to get messages
+        arrayList = new ArrayList<>();
         intent = getIntent();
         String roomId = intent.getStringExtra("roomId");
-        Log.i("roomId!", roomId);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Chats").child(roomId).child("Messages");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    MessageObject messageObject = dataSnapshot1.getValue(MessageObject.class);
+                    arrayList.add(messageObject);
+                }
+                recyclerView = findViewById(R.id.recyclerview_conversation);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                messageAdapter = new MessageAdapter(getApplicationContext(), arrayList);
+                recyclerView.setAdapter(messageAdapter);
+            }
 
-       /* arrayList = new ArrayList<>();
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        //to get names and ids
+        databaseReference = FirebaseDatabase.getInstance().getReference("Chats").child(roomId);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    int i = 0;
+                    if (i == 1) {
+                        ids = dataSnapshot1.getValue(ArrayList.class);
+                    } else if (i == 2) {
+                        names = dataSnapshot1.getValue(ArrayList.class);
+                    }
+                    i++;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+        /*
+        arrayList = new ArrayList<>();
         MessageObject messageObject = new MessageObject("Pablo","This is my message","July 31","2:11pm");
         MessageObject messageObject2 = new MessageObject("Pablo","This is my message","August 1","2:13pm");
         arrayList.add(messageObject);
@@ -43,13 +95,14 @@ public class Conversation extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         messageAdapter = new MessageAdapter(this, arrayList);
         recyclerView.setAdapter(messageAdapter);
-*/
+        */
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        menu.add("Pablo");
-        menu.add("Fernando");
+        for (int i = 0; i < names.size(); i++) {
+            menu.add(names.get(i));
+        }
         inflater.inflate(R.menu.menuinconversation, menu);
         return true;
     }
--- app/src/main/java/pablo/myexample/freechat/SignIn.java
+++ app/src/main/java/pablo/myexample/freechat/SignIn.java
@@ -77,6 +77,9 @@ public class SignIn extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        finish();
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 }
